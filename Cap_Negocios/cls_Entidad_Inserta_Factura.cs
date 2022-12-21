e5fb5586cc92f25004c11c74972662234ccb7856
luisalb29 <114633622+luisalb29@users.noreply.github.com>
2022-12-21 13:43:10 -0600

    clases creadas para insertar facturas y reservas

---
+++ Cap_Negocios/cls_Entidad_Inserta_Factura.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap_Negocios
+{
+    internal class cls_Entidad_Inserta_Factura
+    {
+    }
+}
