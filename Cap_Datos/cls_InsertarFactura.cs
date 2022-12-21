04bf19999f01f8493cebf16c5818ad2a1388ecc8
luisalb29 <114633622+luisalb29@users.noreply.github.com>
2022-12-21 13:58:46 -0600

    insertar factura completado

--- Cap_Datos/cls_InsertarFactura.cs
+++ Cap_Datos/cls_InsertarFactura.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,56 @@ namespace Cap_Datos
 {
     internal class cls_InsertarFactura
     {
+        SqlConnection ObjConex = new SqlConnection();
+        SqlCommand Objcmd = new SqlCommand();
+
+        public void m_Insertar_Factura(int v_COD_FACTURA, int v_FECHA, float v_TOTAL, string v_CED_CLIENTE, string v_HABITACION_FACTURADA, string v_SERVICIO_FACTURADO)
+        {
+            try
+            {
+                ObjConex.ConnectionString = ("Data Source=LAPTOP-UTFDK6V4\\SQLEXPRESS;Initial Catalog=DB_CLUB_CAMPESTRE_UAM;Integrated Security=True");
+
+                SqlParameter p_COD_FACTURA = new SqlParameter("@COD_FACTURA", SqlDbType.Int);
+                p_COD_FACTURA.Value = v_COD_FACTURA;
+
+                SqlParameter p_FECHA = new SqlParameter("@FECHA", SqlDbType.DateTime);
+                p_FECHA.Value = v_FECHA;
+
+                SqlParameter p_TOTAL = new SqlParameter("@TOTAL", SqlDbType.Int);
+                p_TOTAL.Value = v_TOTAL;
+
+                SqlParameter p_CED_CLIENTE = new SqlParameter("@CED_CLIENTE", SqlDbType.Char);
+                p_CED_CLIENTE.Value = v_CED_CLIENTE;
+
+                SqlParameter p_HABITACION_FACTURADA = new SqlParameter("@HABITACION_FACTURADA", SqlDbType.VarChar);
+                p_HABITACION_FACTURADA.Value = v_HABITACION_FACTURADA;
+
+                SqlParameter p_SERVICIO_FACTURADO = new SqlParameter("@SERVICIO_FACTURADO", SqlDbType.NVarChar);
+                p_SERVICIO_FACTURADO.Value = v_SERVICIO_FACTURADO;
+
+                //Agregar parametros
+                Objcmd.Parameters.Add(p_COD_FACTURA);
+                Objcmd.Parameters.Add(p_FECHA);
+                Objcmd.Parameters.Add(p_TOTAL);
+                Objcmd.Parameters.Add(p_CED_CLIENTE);
+                Objcmd.Parameters.Add(p_HABITACION_FACTURADA);
+                Objcmd.Parameters.Add(p_SERVICIO_FACTURADO);
+
+                Objcmd.CommandType = CommandType.StoredProcedure; // Llamada al proceso de almacenado en DB Empleados
+                Objcmd.CommandText = "SP_INSERTAR_FACTURA";
+                ObjConex.Open();
+                Objcmd.Connection = ObjConex;
+                Objcmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                ObjConex.Close();
+            }
+        }
     }
 }
