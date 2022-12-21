8c0087ee3488bf12fd7d68ff265fee9bb3fdb64a
luisalb29 <114633622+luisalb29@users.noreply.github.com>
2022-12-21 14:57:23 -0600

    cambios

---
+++ Cap_Negocios/cls_Entidad_Mostrar_Factura.cs
@@ -0,0 +1,23 @@
+﻿using Cap_Datos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap_Negocios
+{
+    public class cls_Entidad_Mostrar_Factura
+    {
+        int COD_FACTURA;
+
+        public int COD_FACTURA1 { get => COD_FACTURA; set => COD_FACTURA = value; }
+
+        public DataTable m_Consultar_Factura()
+        {
+            cls_MostrarFactura insAD = new cls_MostrarFactura();
+            return insAD.m_Consultar_Factura(COD_FACTURA);
+        }
+    }
+}
