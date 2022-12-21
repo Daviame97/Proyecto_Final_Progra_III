2381b71598e6825f0350650bf89c881ce89eb1c1
luisalb29 <114633622+luisalb29@users.noreply.github.com>
2022-12-21 16:25:45 -0600

    cambio form

--- Cap_GUI/Form1.cs
+++ Cap_GUI/Form1.cs
@@ -16,5 +16,45 @@ namespace Cap_GUI
         {
             InitializeComponent();
         }
+
+        private void btn_AgregarCliente_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Salir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Salir_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Consultar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
     }
 }
