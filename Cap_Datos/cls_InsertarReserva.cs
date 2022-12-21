e7949a33f21b13b2833563fbf00f3c161116c0e0
luisalb29 <114633622+luisalb29@users.noreply.github.com>
2022-12-21 14:18:16 -0600

    Inicio de cambios a reserva

--- Cap_Datos/cls_InsertarReserva.cs
+++ Cap_Datos/cls_InsertarReserva.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,63 @@ namespace Cap_Datos
 {
     internal class cls_InsertarReserva
     {
-    }
+        public class cls_Insertar_Reserva
+        {
+            SqlConnection ObjConex = new SqlConnection();
+            SqlCommand Objcmd = new SqlCommand();
+
+            public void m_Insertar_Reserva(int v_RESERVA_ID, int v_CANT_PERSONAS, int v_FECHA, int v_NUM_HABITACION, string v_CED_CLIENTE, string v_HABITACION_RESERVADA, string v_SERVICIO_RESERVADO)
+            {
+                try
+                {
+                    ObjConex.ConnectionString = ("Data Source=LAPTOP-UTFDK6V4\\SQLEXPRESS;Initial Catalog=DB_CLUB_CAMPESTRE_UAM;Integrated Security=True");
+
+                    SqlParameter p_RESERVA_ID = new SqlParameter("@RESERVA_ID", SqlDbType.Int);
+                    p_RESERVA_ID.Value = v_RESERVA_ID;
+
+                    SqlParameter p_CANT_PERSONAS = new SqlParameter("@CANT_PERSONAS", SqlDbType.TinyInt);
+                    p_CANT_PERSONAS.Value = v_CANT_PERSONAS;
+
+                    SqlParameter p_FECHA = new SqlParameter("@FECHA", SqlDbType.DateTime);
+                    p_FECHA.Value = v_FECHA;
+
+                    SqlParameter p_NUM_HABITACION = new SqlParameter("@NUM_HABITACION", SqlDbType.Int);
+                    p_NUM_HABITACION.Value = v_NUM_HABITACION;
+
+                    SqlParameter p_TEL = new SqlParameter("@CED_CLIENTE", SqlDbType.Char);
+                    p_TEL.Value = v_CED_CLIENTE;
+
+                    SqlParameter p_HABITACION_RESERVADA = new SqlParameter("@HABITACION_RESERVADA", SqlDbType.VarChar);
+                    p_HABITACION_RESERVADA.Value = v_HABITACION_RESERVADA;
+
+                    SqlParameter p_SERVICIO_RESERVADO = new SqlParameter("@SERVICIO_RESERVADO", SqlDbType.NVarChar);
+                    p_SERVICIO_RESERVADO.Value = v_SERVICIO_RESERVADO;
+
+
+                    //Agregar parametros
+                    Objcmd.Parameters.Add(p_RESERVA_ID);
+                    Objcmd.Parameters.Add(p_CANT_PERSONAS);
+                    Objcmd.Parameters.Add(p_FECHA);
+                    Objcmd.Parameters.Add(p_NUM_HABITACION);
+                    Objcmd.Parameters.Add(p_TEL);
+                    Objcmd.Parameters.Add(p_HABITACION_RESERVADA);
+                    Objcmd.Parameters.Add(p_SERVICIO_RESERVADO);
+
+                    Objcmd.CommandType = CommandType.StoredProcedure; // Llamada al proceso de almacenado en DB Empleados
+                    Objcmd.CommandText = "SP_INSERTAR_RESERVAS";
+                    ObjConex.Open();
+                    Objcmd.Connection = ObjConex;
+                    Objcmd.ExecuteNonQuery();
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    ObjConex.Close();
+                }
+            }
+        }
 }
