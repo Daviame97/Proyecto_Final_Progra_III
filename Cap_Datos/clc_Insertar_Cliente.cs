f177c321a01f3ca2acbfd05a1af0cc7699a6ebb6
DAVID EDUARDO ARROYO MESEN <david.arroyo@uamcr.net>
2022-12-21 02:51:39 -0600

    Capas para insertar cliente

--- Cap_Datos/clc_Insertar_Cliente.cs
+++ Cap_Datos/clc_Insertar_Cliente.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Cap_Datos
 {
-    public class Class1
+    internal class clc_Insertar_Cliente
     {
     }
 }
--- Cap_Negocios/cls_Entidad_Insertar_Cliente.cs
+++ Cap_Negocios/cls_Entidad_Insertar_Cliente.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Cap_Datos;
 
 namespace Cap_Negocios
 {
-    public class Class1
+    public class cls_Entidad_Insertar_Cliente
     {
     }
 }
