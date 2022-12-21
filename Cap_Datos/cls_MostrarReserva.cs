6627aaab4a98160b837df14f13a69585de124606
luisalb29 <114633622+luisalb29@users.noreply.github.com>
2022-12-21 15:08:24 -0600

    cambios

--- Cap_Datos/cls_MostrarReserva.cs
+++ Cap_Datos/cls_MostrarReserva.cs
@@ -10,6 +10,29 @@ namespace Cap_Datos
 {
     public class cls_MostrarReserva
     {
-        
+        SqlConnection ObjConex = new SqlConnection();
+        SqlDataAdapter ObjAdaptador;
+        DataTable ObjTB_Reserva;
+
+        public DataTable m_Consultar_Factura(int v_RESERVA_ID)
+        {
+            try
+            {
+                ObjConex.ConnectionString = ("Data Source=LUISBLANCO-PC\\SQL2019_DEV;Initial Catalog=DB_CLUB_CAMPESTRE_UAM;Integrated Security=True");
+                ObjAdaptador = new SqlDataAdapter("SELECT * FROM [SCH_CLUB].[TB_RESERVAS] ='" + v_RESERVA_ID + "' ", ObjConex);
+                ObjTB_Reserva = new DataTable();
+                ObjConex.Open();
+                ObjAdaptador.Fill(ObjTB_Reserva); // Carga la informacion en el "GritView"
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                ObjConex.Close();
+            }
+            return ObjTB_Reserva;
         }
+    }
 }
