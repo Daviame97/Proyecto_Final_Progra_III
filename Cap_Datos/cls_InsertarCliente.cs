1e46a782248351fbaefaa674e1d4dddf19db3023
luisalb29 <114633622+luisalb29@users.noreply.github.com>
2022-12-20 18:29:56 -0600

    metodo insertar cliente

---
+++ Cap_Datos/cls_InsertarCliente.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap_Datos
+{
+    internal class cls_InsertarCliente
+    {
+        SqlConnection ObjConex = new SqlConnection();
+        SqlCommand Objcmd = new SqlCommand();
+        public void m_Insertar_Cliente(string ced, string nombre, string apellido, int edad)
+        {
+            try
+            {
+                ObjConex.ConnectionString = ("Data Source=LAPTOP-UTFDK6V4\\SQLEXPRESS;Initial Catalog=Empleados;Integrated Security=True");
+
+                SqlParameter p_Ced = new SqlParameter("@CED", SqlDbType.Char);
+                p_Ced.Value = ced;
+
+                SqlParameter p_Nombre = new SqlParameter("@NOMBRE", SqlDbType.NVarChar);
+                p_Nombre.Value = nombre;
+
+                SqlParameter p_Apellido = new SqlParameter("@APELLIDO", SqlDbType.NVarChar);
+                p_Apellido.Value = apellido;
+
+                SqlParameter p_Edad = new SqlParameter("@EDAD", SqlDbType.TinyInt);
+                p_Edad.Value = edad;
+
+                
+
+                //Agregar parametros
+                Objcmd.Parameters.Add(p_Ced);
+                Objcmd.Parameters.Add(p_Nombre);
+                Objcmd.Parameters.Add(p_Apellido);
+                Objcmd.Parameters.Add(p_Edad);
+                
+                
+
+                Objcmd.CommandType = CommandType.StoredProcedure; // Llamada al proceso de almacenado en DB Club
+                Objcmd.CommandText = "SP_INSERTAR_CLIENTE";
+                ObjConex.Open();
+                Objcmd.Connection = ObjConex;
+                Objcmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                ObjConex.Close();
+            }
+
+        }
+    }
+}
